6bb7b98bf5bd71ec70d04f32e95fcbe802a0a270
Rukshi101 <rthiyaga@edu.uwaterloo.ca>
2026-03-15 15:36:19 -0400

    Fix test_globals.c missing stddef.h include

--- tests/stubs/test_globals.c
+++ tests/stubs/test_globals.c
@@ -1,5 +1,5 @@
 #include "FreeRTOS.h"
-
+#include <stddef.h>
 /* Definitions of extern variables referenced by velocity_control.c */
 SemaphoreHandle_t hall_semaphore = NULL;
 QueueHandle_t setpoint_queue = NULL;
